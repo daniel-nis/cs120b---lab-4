be38f153b960893603081ed0c87a334443383e80
Daniel Nissan <dniss001@wch136-24.cs.ucr.edu>
2021-04-18 21:25:03 -0700

    part 4

---
+++ lab4/turnin/dniss001_lab4_part4_tests.py
@@ -0,0 +1,25 @@
+tests = [ {'description': 'PINA: 0x04 => PORTB: 0x00',
+    'steps': [ {'inputs': [('PINA',0x04)], 'iterations': 1 } ],
+    'expected': [('PORTB',0x00)],
+    },
+
+	{'description': 'PINA: 0x00, PINA: 0x04, PINA: 0x00, PINA: 0x04, PINA: 0x00, PINA: 0x02, PINA:0x00, PINA: 0x04, PINA: 0x00, PINA: 0x02, PINA: 0x00 => PORTB: 0x00',
+    'steps': [{'inputs': [('PINA',0x00)], 'iterations': 1 },
+ 	{'inputs': [('PINA',0x04)], 'iterations': 1 },
+ 	{'inputs': [('PINA',0x00)], 'iterations': 1 },
+ 	{'inputs': [('PINA',0x04)], 'iterations': 1 },
+ 	{'inputs': [('PINA',0x00)], 'iterations': 1 },
+	{'inputs': [('PINA',0x02)], 'iterations': 1 },
+        {'inputs': [('PINA',0x00)], 'iterations': 1 },
+	{'inputs': [('PINA',0x04)], 'iterations': 1 },
+        {'inputs': [('PINA',0x00)], 'iterations': 1 },
+        {'inputs': [('PINA',0x02)], 'iterations': 1 },
+        {'inputs': [('PINA',0x00)], 'iterations': 1 },
+ 	],
+    'expected': [('PORTB',0x00)],
+    },
+
+	]
+
+#watch = [State]
+
